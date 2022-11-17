1cceff1c66bcb0b2436e5985106af3b4d7dfff58
Augusto Mota <augustomota180798@gmail.com>
2022-11-17 17:49:55 -0300

    verção 1

---
+++ src/Pages/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Formulario from '../Componentes/Formulario';
+import Lista from '../Componentes/Lista';
+
+function App() {
+    return (
+        <div className="AppStyle">
+            <Formulario />
+            <Lista />
+        </div>
+    );
+}
+
+export default App;
